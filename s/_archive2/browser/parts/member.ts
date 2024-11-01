d684bbb242583e88791e342238c83eba970cda61
Chase Moskal <chasemoskal@gmail.com>
2024-11-01 11:34:16 -0700

    obliterate: all archived code

--- s/_archive2/browser/parts/member.ts
+++
@@ -1,9 +0,0 @@
-
-import {PersonInfo} from "../../signaling/parts/people.js"
-
-export class Member {
-	constructor(public info: PersonInfo) {}
-	get id() { return this.info.id }
-	get name() { return this.info.name }
-}
-
