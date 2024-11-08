196b506777d62e40fc79b696c6c68455d2288c58
Chase Moskal <chasemoskal@gmail.com>
2024-11-08 13:21:40 -0800

    tweak: ui

--- s/demo/views/client/view.ts
+++ s/demo/views/client/view.ts
@@ -6,19 +6,20 @@ import {LobbyView} from "../lobby/view.js"
 import {Lobby} from "../../logic/types.js"
 import {Sparrow} from "../../../browser/sparrow.js"
 import {JoinerSituation} from "../../logic/situations/joiner.js"
+import {Id} from "../../../tools/id.js"
 
 export const ClientView = shadowView(use => (situation: JoinerSituation) => {
 	use.styles(stylesCss)
 
 	const lobby = situation.lobby.value
-	const {self, host} = situation.joined
+	const {self, invite} = situation.joined
 
 	const op = lobby
 		? Op.ready(lobby)
 		: Op.loading<Lobby>()
 
 	return html`
-		<h2>Joined <code>${Sparrow.mixedId(host)}</code></h2>
+		<h2>Joined <code>${Id.toFancy(invite)}</code></h2>
 
 		${loading.binary(op, lobby => LobbyView([self.id, lobby]))}
 	`
