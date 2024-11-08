c6dd15faaed3179c9889630b9c8ed4db630a8d24
Chase Moskal <chasemoskal@gmail.com>
2024-11-08 13:26:20 -0800

    tweak: syntax

--- s/demo/views/client/view.ts
+++ s/demo/views/client/view.ts
@@ -1,12 +1,11 @@
 
 import {html, loading, Op, shadowView} from "@benev/slate"
 
+import {Id} from "../../../tools/id.js"
 import stylesCss from "./styles.css.js"
 import {LobbyView} from "../lobby/view.js"
 import {Lobby} from "../../logic/types.js"
-import {Sparrow} from "../../../browser/sparrow.js"
 import {JoinerSituation} from "../../logic/situations/joiner.js"
-import {Id} from "../../../tools/id.js"
 
 export const ClientView = shadowView(use => (situation: JoinerSituation) => {
 	use.styles(stylesCss)
