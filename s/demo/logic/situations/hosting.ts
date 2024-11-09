06d366955da53dce55b2e46413cbec6f7af0864d
Chase Moskal <chasemoskal@gmail.com>
2024-11-08 16:31:33 -0800

    wip: custom experimental cloudflare rtc

--- s/demo/logic/situations/hosting.ts
+++ s/demo/logic/situations/hosting.ts
@@ -6,6 +6,7 @@ import {Id} from "../../../tools/id.js"
 import {Hosted} from "../../../browser/host.js"
 import {Stats} from "../../../signaller/types.js"
 import {Sparrow} from "../../../browser/sparrow.js"
+import {customRtcConfig} from "./custom-rtc-config.js"
 import {Lobby, Person, UserDetails} from "../types.js"
 import {Connection, StdCable} from "../../../browser/types.js"
 
@@ -24,6 +25,7 @@ export class HostingSituation {
 		const onClosed = pubsub()
 
 		const hosted = await Sparrow.host<StdCable>({
+			rtcConfig: customRtcConfig(),
 			url,
 			allow: async() => true,
 			closed: () => {
