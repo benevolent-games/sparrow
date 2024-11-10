3b8f77f3a049121b2b32a37be9a9097a3f922874
Chase Moskal <chasemoskal@gmail.com>
2024-11-09 23:55:25 -0800

    add: more exports

--- s/index.ts
+++ s/index.ts
@@ -1,5 +1,8 @@
 
 export * from "@benev/slate/x/tools/concurrent.js"
+export * from "@benev/slate/x/tools/data/hex.js"
+export * from "@benev/slate/x/tools/data/base58.js"
+export * from "@benev/slate/x/tools/data/bytename.js"
 
 export * from "./browser/std/cable.js"
 export * from "./browser/std/logging.js"
@@ -18,6 +21,8 @@ export * from "./browser/sparrow.js"
 export * from "./browser/types.js"
 
 export * from "./signaller/types.js"
+
+export * from "./tools/id.js"
 
 export {Sparrow as default} from "./browser/sparrow.js"
 
